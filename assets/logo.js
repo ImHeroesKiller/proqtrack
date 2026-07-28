9610e44e22f5711b0b147b384956c4458175b3ac
ImHeroesKiller <148073026+ImHeroesKiller@users.noreply.github.com>
2026-07-28 20:10:14 +0700

    brand: logo module assembler for ProQTrack light wordmark

--- assets/logo.js
+++ assets/logo.js
@@ -1,1 +1,9 @@
-PLACEHOLDER
+import p0 from './logo-part-0.js';
+import p1 from './logo-part-1.js';
+import p2 from './logo-part-2.js';
+import p3 from './logo-part-3.js';
+import p4 from './logo-part-4.js';
+import p5 from './logo-part-5.js';
+import p6 from './logo-part-6.js';
+import p7 from './logo-part-7.js';
+export const LOGO_LIGHT = 'data:image/png;base64,' + p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7;
