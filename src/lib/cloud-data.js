84eb900a0d6783905d6db084f43b8f35ec2fe97b
ImHeroesKiller <148073026+ImHeroesKiller@users.noreply.github.com>
2026-09-22 10:12:00 +0700

    feat(master): include competitor and attendance-point data in cloud bridge

--- src/lib/cloud-data.js
+++ src/lib/cloud-data.js
@@ -14,6 +14,9 @@ export const CLOUD_COLLECTIONS = Object.freeze([
   'surveyTemplates',
   'surveyResponses',
   'projectProducts',
+  'competitors',
+  'competitorProducts',
+  'attendancePoints',
 ]);
 
 const DB_KEY = 'proqtrack_db_v6';
